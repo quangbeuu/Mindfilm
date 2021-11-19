a84fc9fb48541bf35a7bfc6d62c85a53a56f2a6c
Quangvu9501 <77784866+Quangvu9501@users.noreply.github.com>
2021-11-19 09:31:19 +0700

    Version 8

--- assets/js/Sign_in.js
+++ assets/js/Sign_in.js
@@ -36,7 +36,34 @@ let users = [
     },
 ]
 
-// signin 
+
+// 1. Hiện thằng Form đăng nhập
+
+document.getElementsByClassName("signin-btn")[0].addEventListener("click",(e)=>{
+    loginBlock();
+})
+document.getElementsByClassName("signin-btn")[1].addEventListener("click",(e)=>{
+    loginBlock();
+})
+
+function loginBlock(){
+    document.getElementsByClassName("movie__wrap")[0].style.display = "none";
+    document.getElementsByClassName("movie__login")[0].style.display = "block";
+}
+
+function wrapBlock(){
+    document.getElementsByClassName("movie__wrap")[0].style.display = "block";
+    document.getElementsByClassName("movie__login")[0].style.display = "none";
+}
+// 2. Hiện Form đăng ký
+
+document.getElementsByClassName("signup-btn")[0].addEventListener("click",(e)=>{
+    loginBlock();
+    signupBlock();
+})
+
+
+// 3. Tùy biến đăng nhập
 document.querySelector(".movie__signin-form-submit").addEventListener("click",(e)=>{
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value; 
@@ -55,7 +82,16 @@ document.querySelector(".movie__signin-form-submit").addEventListener("click",(e
         if(users[i].username == username && users[i].password == password){
             alert(`Sign up successfully. Hello, ${users[i].username} !`);
             check = true;
-            break;
+            if(check == true){
+                wrapBlock();
+                let menuBtn = document.getElementsByClassName("sigin-menu"); 
+                menuBtn[0].style.visibility="hidden";
+
+                document.getElementsByClassName("signin-slidebar")[0].style.display = "none";
+                document.getElementsByClassName("signup-btn")[0].style.display = "none";
+                document.getElementsByClassName("signout-btn")[0].style.display = "block";
+                document.getElementsByClassName("movie__slidebar-username")[0].innerHTML = `Username: ${username}`
+            }
         }
     }
 
@@ -64,23 +100,69 @@ document.querySelector(".movie__signin-form-submit").addEventListener("click",(e
     }
 })
 
-// Thay đổi giao diện 
+// 4. Thay đổi giao diện Đăng nhập, đăng ký và ngược lại
 document.getElementsByClassName("sign-in")[0].addEventListener("click", function(event){
-    document.getElementsByClassName("movie__signin")[0].style.display = "none";
-    document.getElementsByClassName("movie__signup")[0].style.display = "block";
+    signupBlock();
 })
 
 document.getElementsByClassName("sign-up")[0].addEventListener("click", function(event){
-    document.getElementsByClassName("movie__signup")[0].style.display = "none";
-    document.getElementsByClassName("movie__signin")[0].style.display = "block";
+    signinBlock();
 })
 
+function signupBlock(){
+    document.getElementsByClassName("movie__signin")[0].style.display = "none";
+    document.getElementsByClassName("movie__signup")[0].style.display = "block";
+}
 
-.document.querySelector(".movie__signup-form-submit").addEventListener("click",function(event){
+function signinBlock(){
+    document.getElementsByClassName("movie__signin")[0].style.display = "block";
+    document.getElementsByClassName("movie__signup")[0].style.display = "none";
+}
+
+
+// 5. Tùy biến đăng ký
+document.querySelector(".movie__signup-form-submit").addEventListener("click",function(event){
     let elementError = document.querySelectorAll(".movie__signup error");
     for (let i = 0; i<elementError.length;i++){
         elementError[i].textContent = "";
     }
 
-    let email = document.querySelector("")
+    let username = document.getElementById("username-signup").value;
+    let email = document.getElementById("email-signup").value;
+    let password = document.getElementById("password-signup").value;
+
+    // Kiểm tra hợp lệ
+
+    let check = true; 
+    if(!username){
+        document.getElementById("username-signup").nextElementSibling.textContent = "Username cannot be empty!"
+        check = false;
+    }
+
+    if(!email){
+        document.getElementById("email-signup").nextElementSibling.textContent = "Email cannot be empty!";
+        check = false; 
+    }
+
+    if(!password){
+        document.getElementById("email-signup").nextElementSibling.textContent = "Email cannot be empty!";
+        check = false;
+    }
+
+
+    //Thêm vào mảng user
+    if(check == true){
+        let obj = {
+            id: Math.round(Math.random()*100),
+            username: username,
+            email: email,
+            account: "user",
+            password: password
+        }
+        users.push(obj);
+        alert("Successful account registration!");
+
+        // ẩn đăng ký hiện đăng nhập
+        signinBlock();
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,206 @@
+// 1. Đổ API sang trang html
+// c. Search
+const searchMovie = document.querySelector('#btn-search');
+const inputMovie = document.getElementById("movie__search");
+
+let allFilm = [];
+
+// fullFilm là tập chứa tất cả các film ko phân biệt thể loại
+let fullFilm = []
+
+// + Tìm kiếm khi click vào icon button
+searchMovie.addEventListener("click", (e) => {
+    e.preventDefault();
+    searchEvent();
+})
+
+// + Tìm kiếm khi gõ r ấn enter 
+inputMovie.addEventListener("keyup", (event) => {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        searchEvent()
+    }
+})
+
+function searchEvent() {
+    const searchString = document.querySelector("#movie__search").value.toLowerCase();
+    console.log(allFilm)
+
+    if (searchString !== "") {
+        const filterMovies = fullFilm.filter((film) => {
+            return film.title.toLowerCase().includes(searchString)
+        })
+        console.log(filterMovies);
+        displayFilm(filterMovies);
+    } else {
+        alert("Bạn chưa điền gì!");
+    }
+
+    let movieItem = document.querySelectorAll(".movie__item");
+    document.querySelector(".movie__count").innerHTML = `Đã tìm thấy ${movieItem.length} phim`;
+}
+
+
+// a. Đọc API
+const loadFilms = async () => {
+    try {
+        const api = await fetch("https://api.apify.com/v2/key-value-stores/QubTry45OOCkTyohU/records/LATEST?fbclid=IwAR3oYxXd4dA_aVtsddh6Cprx6ySoKEFeKZp-_v0_y8iF-DBG7GhbwkXFvUw");
+        const responseApi = await api.json();
+        allFilm = responseApi.phim;
+        Object.keys(allFilm).forEach((key) => {
+            allFilm[key].forEach((movie) => {
+                fullFilm.push(movie);
+            })
+        })
+        console.log(fullFilm)
+        console.log(allFilm)
+
+        // Chỉ in ra 8 phim thôi
+        Object.keys(allFilm).forEach((key) => {
+            allFilm[key].length -= allFilm[key].length - 8
+        })
+        displayFilm(allFilm);
+    } catch (err) {
+        console.error(err);
+        console.log("Lỗi kết nối");
+    }
+}
+
+
+
+// b. Đổ lên trang html
+
+const displayFilm = (film) => {
+    console.log(film)
+    let content = document.querySelector(".movie__content")
+    content.innerHTML = "";
+    for (let key in film) {
+        let filmAll = film[key];
+        console.log(filmAll[0])
+        if (!Array.isArray(filmAll)) {
+            let arr = []
+            arr.push(filmAll);
+            filmAll = arr;
+        }
+        content.innerHTML += `
+            <div class="movie__box">
+                <div class="movie__header-wrap">
+                    <h2 class="movie__header">
+                        <i class="fas fa-film"></i>
+                        ${filmAll[0].category.toUpperCase()}
+                    </h2>
+                    <p class="movie__expand">
+                        <a href="phimbo.html">
+                            <i class="fa fa-angle-double-right"></i>
+                            Xem tất cả
+                        </a>
+                    </p>
+                </div>
+                <div class="movie__list" id="${key}">
+                </div>
+            </div>
+        `
+        console.log(key)
+
+        let htmlString = filmAll
+            .map((movie) => {
+                console.log(movie)
+                return `
+                <div class="movie__item">
+                    <img src="${movie.imageUrl}" alt="" class="movie__image">
+                    <h1 class="movie__title">${movie.title}</h1>
+                </div>
+                `
+            })
+            .join("");
+        document.getElementById(key).innerHTML += htmlString;
+
+        console.log(htmlString)
+    }
+}
+
+
+
+loadFilms()
+
+
+// 2. Mở đóng thanh Slidebar
+
+let movieSlidebarOpen = document.getElementById('movie__menu-open');
+let movieSlidebarClose = document.getElementById('movie__slidebar-close');
+let movieSlidebarContainer = document.getElementById('movie__slidebar-container')
+
+function showSlidebar() {
+    movieSlidebarContainer.classList.add('movie__slidebar-open')
+}
+
+movieSlidebarOpen.addEventListener('click', showSlidebar);
+
+function hideSlidebar() {
+    movieSlidebarContainer.classList.remove('movie__slidebar-open')
+}
+
+movieSlidebarClose.addEventListener('click', hideSlidebar);
+
+
+// 3. Làm thanh slider
+
+window.addEventListener("load", function () {
+    const slider = document.querySelector(".movie__slider");
+    const sliderMain = document.querySelector(".movie__slider-main");
+    const sliderItems = document.querySelectorAll(".movie__slider-item")
+    const nextBtn = document.querySelector(".movie__slider-next");
+    const prevBtn = document.querySelector(".movie__slider-prev");
+    const dotItems = document.querySelectorAll(".movie__slider-dot-item");
+
+    // Lấy width của hình ảnh
+    const sliderItemWidth = sliderItems[0].offsetWidth;
+    // Lấy số lượng hình ảnh hiện có
+    const sliderLength = sliderItems.length;
+
+
+
+    let positionX = 0
+    let index = 0
+
+    nextBtn.addEventListener("click", function () {
+        // Nếu là 1 thì next slide
+        handleChangeSlide(1);
+    })
+    prevBtn.addEventListener("click", function () {
+        // Nếu là 1 thì next slide
+        handleChangeSlide(-1);
+    });
+
+    [...dotItems].forEach((item) =>
+        item.addEventListener("click", function (e) {
+            [...dotItems].forEach(el => el.classList.remove("active"));
+            e.target.classList.add("active");
+            const slideIndex = parseInt(e.target.dataset.index);
+            index = slideIndex;
+            positionX = -1 * index * sliderItemWidth;
+            sliderMain.style = `transform: translateX(${positionX}px)`;
+        }));
+
+    function handleChangeSlide(direction) {
+        if (direction === 1) {
+            if (index >= sliderLength - 1) {
+                index = sliderLength - 1;
+                return;
+            }
+            positionX = positionX - sliderItemWidth;
+            sliderMain.style = `transform: translateX(${positionX}px)`
+            index++;
+        } else if (direction === -1) {
+            if (index <= 0) {
+                index = 0;
+                return;
+            }
+            positionX = positionX + sliderItemWidth;
+            sliderMain.style = `transform: translateX(${positionX}px)`
+            index--;
+        }
+        [...dotItems].forEach(el => el.classList.remove("active"));
+        dotItems[index].classList.add("active");
+    }
+})
